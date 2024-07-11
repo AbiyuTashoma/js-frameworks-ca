86b27685a99ceadef03d393f875e74c3a3f9e20c
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-07-11 20:31:26 +0200

    styled product lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AppRoutes from "./components/routes/routes";
+import AppRoutes from "./components/jsx/routes/routes";
 
 function App() {
   return <AppRoutes />;
--- src/components/jsx/data/productsList.jsx
+++ src/components/jsx/data/productsList.jsx
@@ -5,10 +5,10 @@ function Products(products) {
         <Container>
             <Row xs="1" sm="2" lg="4">
                 {products.map((product) =>
-                    <Col key={product.id}>
-                        <img src= {product['image']['url']} />
+                    <Col key={product.id} className="products-col">
+                        <img src= {product['image']['url']} className="list-image"/>
                         <div>Title: {product['title']}</div>
-                        <div>Price: {product['price']}</div>
+                        <div>Price: {product['discountedPrice']}</div>
                     </Col>
                 )};
             </Row>
