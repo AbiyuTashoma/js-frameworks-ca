348ec539fc9fa664d104a52b75a52cf112837455
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-07-14 12:05:57 +0200

    added product details and reviews

--- src/components/jsx/data/price.jsx
+++ src/components/jsx/data/price.jsx
@@ -2,10 +2,11 @@ import { StyledPrice, StyledDiscountedPrice } from "../styled/styled"
 
 function DisplayPrice(ordPrice, discPrice) {
     const isdiscount = Boolean(ordPrice > discPrice)
+    const currency = 'NOK';
     return (
         <div> 
-            <StyledPrice isDiscounted = { isdiscount }> { ordPrice } </StyledPrice> 
-            { isdiscount ? <StyledDiscountedPrice isDiscounted={ isdiscount }> { discPrice } </StyledDiscountedPrice> : ''}
+            <div> { discPrice } {currency}</div> 
+            { isdiscount ? <StyledDiscountedPrice isDiscounted={ isdiscount }> ({Math.round (discPrice - ordPrice)} {currency})</StyledDiscountedPrice> : ''}
         </div>
     );
 }
--- src/components/jsx/styled/styled.jsx
+++ src/components/jsx/styled/styled.jsx
@@ -3,7 +3,3 @@ import styled from "styled-components";
 export const StyledDiscountedPrice = styled.span`
 color: ${(props) => (props.isDiscounted ? 'red' : '')};
 `;
-
-export const StyledPrice = styled.span`
-text-decoration: ${(props) => (props.isDiscounted ? 'line-through' : '')};
-`;
