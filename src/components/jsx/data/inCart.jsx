1cad9c70e4c28313adc9902529386123fee53ff8
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-07-19 13:44:42 +0200

    updated remove button status

--- src/components/jsx/data/inCart.jsx
+++ src/components/jsx/data/inCart.jsx
@@ -3,22 +3,9 @@ import DisplayPrice from "./price";
 function InCart (cart, product) {
 
     let itemIndex = cart.findIndex((item) => product.id === item.id);
-    // productIndex = cartCopy.findIndex((product) => product.id === anItem.id);
 
     return ( itemIndex >= 0 &&
         <div className="incart">
-            {/* <table>
-                <tbody>
-                    <tr>
-                        <th>In cart:</th>
-                        <td>{ cart[itemIndex].quantity }</td>
-                    </tr>
-                    <tr>
-                        <th>Sub-total:</th>
-                        <td>{DisplayPrice(product.price, product.discountedPrice, cart[itemIndex].quantity)}</td>
-                    </tr>
-                </tbody>
-            </table> */}
             <div>
                 <div>In cart:</div>
                 <div>{ cart[itemIndex].quantity }</div></div>
---
+++ src/components/jsx/data/js/inArray.js
@@ -0,0 +1,4 @@
+export function inArray(items, item) {
+  const itemIndex = items.findIndex((element) => element.id === item.id);
+  return itemIndex >= 0;
+}
