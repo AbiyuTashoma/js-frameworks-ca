88b117567f6148094ccfe2202cacd5b5d825f9a1
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-07-24 14:06:00 +0200

    refactored api to use zustand

--- src/components/jsx/data/productsData.jsx
+++ src/components/jsx/data/productsData.jsx
@@ -3,11 +3,64 @@ import Loading from "./loading";
 import Error from "./error";
 import ProductsList from "./productsList";
 import { url } from "./js/constants";
+import useApi from "../store/apiStore";
+import { shallow } from "zustand/shallow";
+
+export const object = [
+  {
+    id: "109566af-c5c2-4f87-86cb-76f36fb8d378",
+    title: "Vanilla Perfume",
+    description:
+      "Women's perfume that smells a warm and sweet, with nuances of wood and jasmine.",
+    price: 2599.99,
+    discountedPrice: 2079.99,
+    image: {
+      url: "https://static.noroff.dev/api/online-shop/1-perfume-white.jpg",
+      alt: "",
+    },
+    rating: 5,
+    tags: ["perfume", "beauty"],
+    reviews: [
+      {
+        id: "90a61e3e-355a-42e4-b038-d91dcad33c20",
+        username: "Jim N.",
+        rating: 5,
+        description: "My partner loves it, its her favourite.",
+      },
+    ],
+  },
+  {
+    id: "10d6cc02-b282-46bb-b35c-dbc4bb5d91d9",
+    title: "Toy car",
+    description:
+      "A die-cast model of a toy car, perfect for displaying on your shelf.",
+    price: 499.95,
+    discountedPrice: 449.95,
+    image: {
+      url: "https://static.noroff.dev/api/online-shop/12-toy-car.jpg",
+      alt: "",
+    },
+    rating: 0,
+    tags: ["toy"],
+    reviews: [],
+  },
+];
 
 function ProductsData() {
-  const { data, isLoading, isError } = GetApi(url);
 
-  if (isLoading || !data) {
+  const { products, isLoading, isError, updateProducts } = useApi(
+    (state) => ({
+      products: state.products,
+      isLoading: state.isLoading,
+      isError: state.isError,
+      updateProducts: state.updateProducts,
+    }),
+    shallow
+  );
+
+  GetApi(url);
+
+  if (isLoading || !products) {
     return <Loading />;
   }
 
@@ -15,9 +68,12 @@ function ProductsData() {
     return <Error />;
   }
 
-  console.log(data['data']);
-
-  return ProductsList(data['data']);
+  console.log(products);
+  return (
+    <div>
+      <button onClick={() => updateProducts(object)}>Change list</button>
+      {ProductsList(products)}
+    </div>);
 }
 
 export default ProductsData;
